d75513cf4750cfca5fbc7174799567b33da97aff
Alexandre Lacour <alexandre.lacour04@gmail.com>
2025-05-26 09:50:36 +0200

    Refactor ActivityController and simplify API checker logic
    Removed redundant comments and unused code in ActivityController for improved readability and maintainability. In the API checker, optimized data structure formatting, reduced verbosity, and streamlined request handling to enhance code clarity.

--- TP5 Sécurisation de l'accès à l'API REST/sporttrack-spring-boot/api_checker.py
+++ TP5 Sécurisation de l'accès à l'API REST/sporttrack-spring-boot/api_checker.py
@@ -9,18 +9,16 @@ License: GPLv3
 import pycurl
 import json
 import io
-
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # API URL
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 USER = "r401"
 PASSWORD = "But2R041"
 API_URL = f"http://{USER}:{PASSWORD}@localhost:8081/activities/"
 
-
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # HTTP Response codes
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 class HTTP_RESPONSE_CODES:
     STATUS_CODE_400 = "Bad Request"
     STATUS_CODE_401 = "Unauthorized"
@@ -29,7 +27,6 @@ class HTTP_RESPONSE_CODES:
     STATUS_CODE_405 = "Method Not Allowed"
     STATUS_CODE_409 = "Conflict"
 
-
 class API_RESPONSE_CODE:
     SUCCESS = "success"
     FAILURE = "failure"
@@ -37,10 +34,9 @@ class API_RESPONSE_CODE:
     UNVALID_DATA = "unvalid data"
     NO_DATA = "data does not exist"
 
-
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # Validation data
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 valid_data_1 = {
     "date": "01/01/2025",
     "description": "act1",
@@ -48,49 +44,13 @@ valid_data_1 = {
     "freq_max": 103,
     "freq_min": 98,
     "data": [
-        {
-            "time": "13:00:00",
-            "cardio_frequency": 99,
-            "latitude": 47.644795,
-            "longitude": -2.776605,
-            "altitude": 18,
-        },
-        {
-            "time": "13:00:05",
-            "cardio_frequency": 100,
-            "latitude": 47.646870,
-            "longitude": -2.778911,
-            "altitude": 18,
-        },
-        {
-            "time": "13:00:10",
-            "cardio_frequency": 102,
-            "latitude": 47.646197,
-            "longitude": -2.780220,
-            "altitude": 18,
-        },
-        {
-            "time": "13:00:15",
-            "cardio_frequency": 100,
-            "latitude": 47.646992,
-            "longitude": -2.781068,
-            "altitude": 17,
-        },
-        {
-            "time": "13:00:20",
-            "cardio_frequency": 98,
-            "latitude": 47.647867,
-            "longitude": -2.781744,
-            "altitude": 16,
-        },
-        {
-            "time": "13:00:25",
-            "cardio_frequency": 103,
-            "latitude": 47.648510,
-            "longitude": -2.780145,
-            "altitude": 16,
-        },
-    ],
+        {"time": "13:00:00", "cardio_frequency": 99, "latitude": 47.644795, "longitude": -2.776605, "altitude": 18},
+        {"time": "13:00:05", "cardio_frequency": 100, "latitude": 47.646870, "longitude": -2.778911, "altitude": 18},
+        {"time": "13:00:10", "cardio_frequency": 102, "latitude": 47.646197, "longitude": -2.780220, "altitude": 18},
+        {"time": "13:00:15", "cardio_frequency": 100, "latitude": 47.646992, "longitude": -2.781068, "altitude": 17},
+        {"time": "13:00:20", "cardio_frequency": 98, "latitude": 47.647867, "longitude": -2.781744, "altitude": 16},
+        {"time": "13:00:25", "cardio_frequency": 103, "latitude": 47.648510, "longitude": -2.780145, "altitude": 16}
+    ]
 }
 
 
@@ -101,21 +61,9 @@ valid_data_2 = {
     "freq_max": 103,
     "freq_min": 98,
     "data": [
-        {
-            "time": "13:00:00",
-            "cardio_frequency": 99,
-            "latitude": 47.644795,
-            "longitude": -2.776605,
-            "altitude": 18,
-        },
-        {
-            "time": "13:00:25",
-            "cardio_frequency": 103,
-            "latitude": 47.648510,
-            "longitude": -2.780145,
-            "altitude": 16,
-        },
-    ],
+        {"time": "13:00:00", "cardio_frequency": 99, "latitude": 47.644795, "longitude": -2.776605, "altitude": 18},
+        {"time": "13:00:25", "cardio_frequency": 103, "latitude": 47.648510, "longitude": -2.780145, "altitude": 16}
+    ]
 }
 
 unvalid_data_1 = {
@@ -125,21 +73,9 @@ unvalid_data_1 = {
     "freq_max": 103,
     "freq_min": 98,
     "data": [
-        {
-            "time": "13:00:00",
-            "cardio_frequency": -1,
-            "latitude": 47.644795,
-            "longitude": -2.776605,
-            "altitude": 18,
-        },
-        {
-            "time": "13:00:25",
-            "cardio_frequency": 400,
-            "latitude": 47.648510,
-            "longitude": -2.780145,
-            "altitude": 16,
-        },
-    ],
+        {"time": "13:00:00", "cardio_frequency": -1, "latitude": 47.644795, "longitude": -2.776605, "altitude": 18},
+        {"time": "13:00:25", "cardio_frequency": 400, "latitude": 47.648510, "longitude": -2.780145, "altitude": 16}
+    ]
 }
 
 unvalid_data_2 = {
@@ -149,21 +85,9 @@ unvalid_data_2 = {
     "freq_max": 103,
     "freq_min": 98,
     "data": [
-        {
-            "time": "13:00:00",
-            "cardio_frequency": 180,
-            "latitude": 147.644795,
-            "longitude": -2.776605,
-            "altitude": 18,
-        },
-        {
-            "time": "13:00:25",
-            "cardio_frequency": 103,
-            "latitude": 47.648510,
-            "longitude": -222.780145,
-            "altitude": 16,
-        },
-    ],
+        {"time": "13:00:00", "cardio_frequency": 180, "latitude": 147.644795, "longitude": -2.776605, "altitude": 18},
+        {"time": "13:00:25", "cardio_frequency": 103, "latitude": 47.648510, "longitude": -222.780145, "altitude": 16}
+    ]
 }
 
 unvalid_data_3 = {
@@ -173,21 +97,9 @@ unvalid_data_3 = {
     "freq_max": 300,
     "freq_min": -1,
     "data": [
-        {
-            "time": "13:00:00",
-            "cardio_frequency": 99,
-            "latitude": 47.644795,
-            "longitude": -2.776605,
-            "altitude": 18,
-        },
-        {
-            "time": "13:00:25",
-            "cardio_frequency": 103,
-            "latitude": 47.648510,
-            "longitude": -2.780145,
-            "altitude": 16,
-        },
-    ],
+        {"time": "13:00:00", "cardio_frequency": 99, "latitude": 47.644795, "longitude": -2.776605, "altitude": 18},
+        {"time": "13:00:25", "cardio_frequency": 103, "latitude": 47.648510, "longitude": -2.780145, "altitude": 16}
+    ]
 }
 
 unvalid_data_4 = {
@@ -197,182 +109,154 @@ unvalid_data_4 = {
     "freq_max": 100,
     "freq_min": 80,
     "data": [
-        {
-            "time": "13:00:00",
-            "cardio_frequency": 99,
-            "latitude": 47.644795,
-            "longitude": -2.776605,
-            "altitude": 18,
-        },
-        {
-            "time": "13:00:25",
-            "cardio_frequency": 103,
-            "latitude": 47.648510,
-            "longitude": -2.780145,
-            "altitude": 16,
-        },
-    ],
+        {"time": "13:00:00", "cardio_frequency": 99, "latitude": 47.644795, "longitude": -2.776605, "altitude": 18},
+        {"time": "13:00:25", "cardio_frequency": 103, "latitude": 47.648510, "longitude": -2.780145, "altitude": 16}
+    ]
 }
 
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # Requests
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # the file must be empty
-test_0 = {
+test_0={
     "desc": "Check if file is empty",
     "method": "GET",
     "url": API_URL,
-    "request_payload": None,
-    "response_code": 200,
-    "response_payload": [],
+    "request_payload":None,
+    "response_code":200,
+    "response_payload":[]
 }
 
-test_1 = {
+test_1={
     "desc": "Add a new element",
     "method": "POST",
     "url": API_URL,
-    "request_payload": valid_data_1,
-    "response_code": 200,
-    "response_payload": API_RESPONSE_CODE.SUCCESS,
+    "request_payload":valid_data_1,
+    "response_code":200,
+    "response_payload": API_RESPONSE_CODE.SUCCESS
 }
 
-test_2 = {
+test_2={
     "desc": "Add a new element",
     "method": "POST",
     "url": API_URL,
     "request_payload": valid_data_2,
-    "response_code": 200,
-    "response_payload": API_RESPONSE_CODE.SUCCESS,
+    "response_code":200,
+    "response_payload": API_RESPONSE_CODE.SUCCESS
 }
 
 # data already added in test_2
-test_3 = {
+test_3={
     "desc": "Add an element that is already present in the file",
     "method": "POST",
     "url": API_URL,
     "request_payload": valid_data_2,
-    "response_code": 409,
-    "response_payload": API_RESPONSE_CODE.FAILURE,
+    "response_code":409,
+    "response_payload": API_RESPONSE_CODE.FAILURE
 }
 # data inserted in test1 and test 2
-test_4 = {
+test_4={
     "desc": "Check if data have been inserted successfuly",
     "method": "GET",
     "url": API_URL,
-    "request_payload": None,
-    "response_code": 200,
-    "response_payload": [valid_data_1, valid_data_2],
+    "request_payload":None,
+    "response_code":200,
+    "response_payload":[valid_data_1, valid_data_2]
 }
 
 # unvalid data
-test_5 = {
+test_5={
     "desc": "Add a unvalid data",
     "method": "POST",
     "url": API_URL,
-    "request_payload": unvalid_data_1,
-    "response_code": 400,
-    "response_payload": API_RESPONSE_CODE.UNVALID_DATA,
+    "request_payload":unvalid_data_1,
+    "response_code":400,
+    "response_payload": API_RESPONSE_CODE.UNVALID_DATA
 }
 
-test_6 = {
+test_6={
     "desc": "Add a unvalid data",
     "method": "POST",
     "url": API_URL,
-    "request_payload": unvalid_data_2,
-    "response_code": 400,
-    "response_payload": API_RESPONSE_CODE.UNVALID_DATA,
+    "request_payload":unvalid_data_2,
+    "response_code":400,
+    "response_payload": API_RESPONSE_CODE.UNVALID_DATA
 }
 
-test_7 = {
+test_7={
     "desc": "Add a unvalid data",
     "method": "POST",
     "url": API_URL,
-    "request_payload": unvalid_data_3,
-    "response_code": 400,
-    "response_payload": API_RESPONSE_CODE.UNVALID_DATA,
+    "request_payload":unvalid_data_3,
+    "response_code":400,
+    "response_payload": API_RESPONSE_CODE.UNVALID_DATA
 }
 
-test_8 = {
+test_8={
     "desc": "Add a unvalid data",
     "method": "POST",
     "url": API_URL,
-    "request_payload": unvalid_data_4,
-    "response_code": 400,
-    "response_payload": API_RESPONSE_CODE.UNVALID_DATA,
+    "request_payload":unvalid_data_4,
+    "response_code":400,
+    "response_payload": API_RESPONSE_CODE.UNVALID_DATA
 }
 # we try to make a post on path activities/act1
-test_9 = {
+test_9={
     "desc": "Unauthorized operation",
     "method": "POST",
     "url": f"{API_URL}act1",
-    "request_payload": valid_data_1,
-    "response_code": 405,
-    "response_payload": API_RESPONSE_CODE.UNAUTORIZED_OPS,
+    "request_payload":valid_data_1,
+    "response_code":405,
+    "response_payload": API_RESPONSE_CODE.UNAUTORIZED_OPS
 }
 
 # we delete act1
-test_10 = {
+test_10={
     "desc": "Remove an element that is present in the file",
     "method": "DELETE",
     "url": f"{API_URL}act1",
-    "request_payload": None,
-    "response_code": 200,
-    "response_payload": API_RESPONSE_CODE.SUCCESS,
+    "request_payload":None,
+    "response_code":200,
+    "response_payload": API_RESPONSE_CODE.SUCCESS
 }
 # we delete act1 again
-test_11 = {
+test_11={
     "desc": "Remove an element that is not present in the file",
     "method": "DELETE",
     "url": f"{API_URL}act1",
-    "request_payload": "",
-    "response_code": 404,
-    "response_payload": API_RESPONSE_CODE.NO_DATA,
+    "request_payload":"",
+    "response_code":404,
+    "response_payload": API_RESPONSE_CODE.NO_DATA
 }
 # we delete the list
-test_12 = {
+test_12={
     "desc": "Remove all the elements",
     "method": "DELETE",
     "url": f"{API_URL}",
-    "request_payload": "",
-    "response_code": 200,
-    "response_payload": API_RESPONSE_CODE.SUCCESS,
+    "request_payload":"",
+    "response_code":200,
+    "response_payload": API_RESPONSE_CODE.SUCCESS
 }
 
-requests = [
-    test_0,
-    test_1,
-    test_2,
-    test_3,
-    test_4,
-    test_5,
-    test_6,
-    test_7,
-    test_8,
-    test_9,
-    test_10,
-    test_11,
-    test_12,
-]
-
-
-# ------------------------------------------------------------------------------
+requests=[test_0, test_1, test_2, test_3, test_4, test_5, test_6, test_7,
+          test_8, test_9, test_10, test_11, test_12]
+#------------------------------------------------------------------------------
 # Colors
-# ------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 class bcolors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # functions
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 def validate_coordinates(data):
     for entry in data:
         lat = entry.get("latitude")
@@ -382,7 +266,6 @@ def validate_coordinates(data):
         if not (-180 <= lon <= 180):
             raise ValueError(f"Invalid longitude: {lon}")
     print("All GPS coordinates are valid.")
-
 
 def validate_heart_rate(data):
     for entry in data:
@@ -396,34 +279,32 @@ def validate_heart_rate(data):
 def check_json_body(expected, returned):
     expected = json.dumps(expected, sort_keys=True, indent=2, ensure_ascii=False)
     returned = json.dumps(returned, sort_keys=True, indent=2, ensure_ascii=False)
-    return expected == returned
+    return (expected == returned)
 
-
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # HTTP request using cURL
-# -------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------
 
 def http_request(method, url, payload=None):
     buffer = io.BytesIO()
     c = pycurl.Curl()
     c.setopt(c.URL, url)
     c.setopt(c.WRITEDATA, buffer)
-    c.setopt(pycurl.HTTPHEADER, ["Accept: application/json"])
+    c.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
     c.setopt(c.VERBOSE, False)  # Enables detailed request output
 
-    if method.upper() in ["POST", "PUT"]:
+    if method.upper() in ['POST', 'PUT']:
         c.setopt(c.CUSTOMREQUEST, method.upper())
-        json_payload = json.dumps(payload, ensure_ascii=False).encode("utf-8")
+        json_payload = json.dumps(payload, ensure_ascii=False).encode('utf-8')
         c.setopt(c.POSTFIELDS, json_payload)
-        c.setopt(c.HTTPHEADER, ["Content-Type: application/json; charset=utf-8"])
-    elif method.upper() == "DELETE":
-        c.setopt(c.CUSTOMREQUEST, "DELETE")
-    elif method.upper() == "GET":
+        c.setopt(c.HTTPHEADER, ['Content-Type: application/json; charset=utf-8'])
+    elif method.upper() == 'DELETE':
+        c.setopt(c.CUSTOMREQUEST, 'DELETE')
+    elif method.upper() == 'GET':
         pass  # Default is GET
     else:
         raise ValueError(f"Unsupported HTTP method: {method}")
-
+    
     # print request
     c.setopt(pycurl.DEBUGFUNCTION, lambda t, s: print(f"DEBUG: {s.decode()}"))
 
@@ -432,7 +313,7 @@ def http_request(method, url, payload=None):
         status_code = c.getinfo(pycurl.RESPONSE_CODE)
         c.close()
 
-        body = json.loads(buffer.getvalue().decode("utf-8"))
+        body = buffer.getvalue().decode('utf-8')
         # print(f"\n{method.upper()} {url}")
         # print(f"Status Code: {status_code}")
         # print("Response:", body)
@@ -442,26 +323,24 @@ def http_request(method, url, payload=None):
         return None, None
 
 
-# -------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
 # Main
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     score = 0
     request_number = len(requests)
     for req in requests:
         print(f"{bcolors.OKGREEN}{req['desc']}{bcolors.ENDC}")
-        status_code, resp_body = http_request(
-            req["method"], req["url"], req["request_payload"]
-        )
+        status_code, resp_body = http_request(req["method"],req["url"],req["request_payload"]);
         if status_code == req["response_code"]:
             score = score + 1
+            if req["method"] == 'GET':
+                resp_body = json.loads(resp_body)
+
             body_check = check_json_body(req["response_payload"], resp_body)
-            print(
-                f"{bcolors.OKBLUE}Request: {req['method']} {req['url']} -> Response code:{status_code}, body valid: {body_check} {bcolors.ENDC}"
-            )
+            print(f"{bcolors.OKBLUE}Request: {req['method']} {req['url']} -> Response code:{status_code}, body valid: {body_check} {bcolors.ENDC}")
         else:
-            print(
-                f"{bcolors.FAIL}Request: {req['method']} {req['url']} -> Expected status code: {req['response_code']}, returned status code: {status_code} {bcolors.ENDC}"
-            )
+            print(f"{bcolors.FAIL}Request: {req['method']} {req['url']} -> Expected status code: {req['response_code']}, returned status code: {status_code} {bcolors.ENDC}")
     print(f"\n{bcolors.OKGREEN} Score={score}/{request_number}{bcolors.ENDC}")
--- TP5 Sécurisation de l'accès à l'API REST/sporttrack-spring-boot/src/main/java/fr/ubs/sporttrack/controller/ActivityController.java
+++ TP5 Sécurisation de l'accès à l'API REST/sporttrack-spring-boot/src/main/java/fr/ubs/sporttrack/controller/ActivityController.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/activities")
 public class ActivityController {
 
-    private static final String DATA_FILE = "data.json"; // Fichier JSON contenant les activités
+    private static final String DATA_FILE = "data.json";
     private final Validator validator;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -29,94 +29,68 @@ public class ActivityController {
         this.validator = factory.getValidator();
     }
 
-    /**
-     * Récupérer toutes les activités.
-     */
     @GetMapping("/")
     public ResponseEntity<List<Activity>> findAll() {
         try {
             List<Activity> activities = readActivitiesFromFile();
             activities.sort(Comparator.comparing(a -> a.getDescription().toLowerCase()));
-            System.out.println(activities);
-            return ResponseEntity.status(HttpStatus.OK).body(activities);
+            return ResponseEntity.ok(activities);
         } catch (IOException e) {
             System.err.println("Erreur lors de la lecture du fichier: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(Collections.emptyList());
+            return ResponseEntity.ok(new ArrayList<>());
         }
     }
 
-    /**
-     * Ajouter une activité.
-     */
     @PostMapping("/")
     public ResponseEntity<String> addActivity(@RequestBody Activity newActivity) {
-        // Validation des données avec Hibernate Validator
-        Set<ConstraintViolation<Activity>> violations = validator.validate(newActivity);
-        if (!violations.isEmpty()) {
-            String errorMessage = violations.stream()
-                    .map(v -> v.getPropertyPath() + ": " + v.getMessage())
-                    .collect(Collectors.joining(", "));
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("\"" + errorMessage + "\"");
-        }
-
         if (newActivity == null || newActivity.getDescription() == null || newActivity.getDescription().trim().isEmpty()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("\"unvalid data\"");
+                    .body("unvalid data");
         }
 
         // Validation des données
-        if (newActivity.getFreqMin() < 0 || newActivity.getFreqMax() > 250 ||
-                newActivity.getData().stream().anyMatch(d -> d.getCardioFrequency() < 0 || d.getCardioFrequency() > 250) ||
+        if (newActivity.getFreqMin() < 15 || newActivity.getFreqMax() > 220 ||
+                newActivity.getData().stream().anyMatch(d -> d.getCardioFrequency() < 15 || d.getCardioFrequency() > 220) ||
                 newActivity.getData().stream().anyMatch(d -> Math.abs(d.getLatitude()) > 90 || Math.abs(d.getLongitude()) > 180)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("\"unvalid data\"");
+                    .body("unvalid data");
         }
 
         try {
             List<Activity> activities = readActivitiesFromFile();
 
-            // Vérification des doublons
             boolean exists = activities.stream()
                     .anyMatch(activity -> activity.getDescription().trim()
                             .equalsIgnoreCase(newActivity.getDescription().trim()));
 
             if (exists) {
                 return ResponseEntity.status(HttpStatus.CONFLICT)
-                        .body("\"failure\"");
+                        .body("failure");
             }
 
-            // Ajout de l'activité
             activities.add(newActivity);
             writeActivitiesToFile(activities);
 
-            return ResponseEntity.ok("\"success\"");
+            return ResponseEntity.ok("success");
         } catch (IOException e) {
             System.err.println("Erreur lors de l'écriture du fichier: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("\"failure\"");
+                    .body("failure");
         }
     }
 
-    /**
-     * Supprimer toutes les activités.
-     */
     @DeleteMapping("/")
     public ResponseEntity<String> deleteAllActivities() {
         try {
             Files.writeString(new File(DATA_FILE).toPath(), "[]");
-            return ResponseEntity.ok("\"success\"");
+            return ResponseEntity.ok("success");
         } catch (IOException e) {
             System.err.println("Erreur lors de la suppression du fichier: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("\"failure\"");
+                    .body("failure");
         }
     }
 
-    /**
-     * Supprimer une activité spécifique par description.
-     */
     @DeleteMapping("/{desc}")
     public ResponseEntity<String> deleteActivity(@PathVariable String desc) {
         try {
@@ -126,30 +100,24 @@ public class ActivityController {
 
             if (removed) {
                 writeActivitiesToFile(activities);
-                return ResponseEntity.ok("\"success\"");
+                return ResponseEntity.ok("success");
             } else {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body("\"data does not exist\"");
+                        .body("data does not exist");
             }
         } catch (IOException e) {
             System.err.println("Erreur lors de la suppression de l'activité: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("\"failure\"");
+                    .body("failure");
         }
     }
 
-    /**
-     * Gérer les appels POST non autorisés sur /activities/{description}.
-     */
     @PostMapping("/{description}")
     public ResponseEntity<String> unauthorizedOperation(@PathVariable String description) {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
-                .body("\"unauthorized operation\"");
+                .body("unauthorized operation");
     }
 
-    /**
-     * Lire les activités depuis un fichier JSON.
-     */
     private List<Activity> readActivitiesFromFile() throws IOException {
         File file = new File(DATA_FILE);
 
@@ -164,9 +132,6 @@ public class ActivityController {
         );
     }
 
-    /**
-     * Écrire les activités dans un fichier JSON.
-     */
     private void writeActivitiesToFile(List<Activity> activities) throws IOException {
         objectMapper.writeValue(new File(DATA_FILE), activities);
     }
